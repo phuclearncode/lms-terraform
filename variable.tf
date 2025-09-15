47eeddaf412ed10425528c5208bd25f483b72647
phuclearncode <hphucxma2004@gmail.com>
2025-09-15 19:55:11 +0700

    feat(cluster): create cluster with 1 masternode and 2 worker node

--- variable.tf
+++ variable.tf
@@ -38,3 +38,18 @@ variable "private_subnets" {
   description = "A list of private subnet CIDR blocks"
   type        = list(string)
 }
+
+# Region to build in
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+# Names of the EC2 instances to create
+locals {
+  instances = [
+    "controlplane",
+    "node01",
+    "node02"
+  ]
+}
